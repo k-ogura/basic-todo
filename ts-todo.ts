3fe647bf67ee50d1f2b30e8e0c737fd30ff96f5a
Ogura Katsuya <k-ogura@ist.osaka-u.ac.jp>
2018-02-27 15:31:52 +0900

    雛形ができました

--- ts-todo.ts
+++ ts-todo.ts
@@ -1,1 +1,52 @@
-console.log("Hello, World!");
+interface Todo
+{
+    content: string;
+    id: number;
+}
+
+class TodoControl implements Todo
+{
+    content : "やること";
+    id : 114514;
+
+    complete(){
+        console.log("complete todo");
+    }
+
+    add ()
+    {
+        console.log("add todo");
+    }
+
+    show()
+    {
+        console.log("show todo");
+    }
+}
+
+declare let process: any;
+const todo = new TodoControl();
+
+if (process.argv.length >= 3)
+{
+    let cmd: string = process.argv[2];
+    if (cmd == "add")
+    {
+        if (process.argv.length >= 4)
+        {
+            todo.add();
+        }
+    }
+    else
+    if (cmd == "complete")
+    {
+        if (process.argv.length >= 4)
+        {
+            todo.complete();
+        }
+    }
+}
+else
+{
+    todo.show();
+}
